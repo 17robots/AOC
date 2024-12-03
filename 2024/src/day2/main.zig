cb57021428162eba24249e6d0462f2ba96680e61
17robots <mdray@duck.com>
2024-12-02 20:13:45 -0500

    fix: redo for part 2

--- 2024/src/day2/main.zig
+++ 2024/src/day2/main.zig
@@ -8,44 +8,19 @@ pub fn main() !void {
     defer arena.deinit();
     const alloc = arena.allocator();
     var safes: u32 = 0;
+    safes = 1;
     var lines = std.mem.tokenizeAny(u8, try util.read_file(alloc, "src/day2/2.txt"), "\n\r");
     while (lines.next()) |line| {
-        var numbers = std.mem.tokenizeAny(u8, line, " ");
-        var asc: ?bool = null;
         var prev: ?i32 = null;
-        var unsafes: u32 = 0;
-        while (numbers.next()) |n| blk: {
+        var currentDif: ?i32 = null;
+        var numbers = std.mem.tokenizeAny(u8, line, " ");
+        while (numbers.next()) |n| {
             const x = try std.fmt.parseInt(i32, n, 10);
-            var skip_x = false;
             if (prev) |p| {
-                if (asc) |a| {
-                    switch (a) {
-                        true => if (p > x) {
-                            if (unsafes > tolerance) break :blk;
-                            unsafes += 1;
-                            skip_x = true;
-                        },
-                        false => if (p < x) {
-                            if (unsafes > tolerance) break :blk;
-                            unsafes += 1;
-                            skip_x = true;
-                        },
-                    }
-                } else {
-                    asc = p < x;
-                }
-                switch (@abs(x - p)) {
-                    1...range => {},
-                    else => {
-                        if (unsafes > tolerance) break :blk;
-                        unsafes += 1;
-                        skip_x = true;
-                    },
-                }
+                currentDif = x - p;
             }
-            if (!skip_x) prev = x;
+            prev = x;
         }
-        safes += if (unsafes > 1) 0 else 1;
     }
-    std.debug.print("safes: {}", .{safes});
+    std.debug.print("safes: {}\n", .{safes});
 }
--- 2024/src/util.zig
+++ 2024/src/util.zig
@@ -5,3 +5,9 @@ pub fn read_file(a: std.mem.Allocator, filename: []const u8) ![]const u8 {
     const stat = try file.stat();
     return try file.readToEndAlloc(a, stat.size);
 }
+pub fn all(comptime t: type, list: []t, func: anytype) bool {
+    for (list) |i| {
+        if (!@call(.auto, func, .{i})) return false;
+    }
+    return true;
+}
